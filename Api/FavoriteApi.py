e63e7401b3ab81ea6ae823330310863a08449dc2
Baptiste-Boursin <baptiste.boursin@epsi.fr>
2021-03-09 14:58:09 +0100

    adding token verification

--- Api/FavoriteApi.py
+++ Api/FavoriteApi.py
@@ -1,46 +1,60 @@
 from flask import Blueprint,request,jsonify
 import DatabaseFiles.connection as connection
+from VerifToken import verifyToken
+
 
 favorite_api = Blueprint('favorite_api',__name__)
 
 
-@favorite_api.route('/favorite/<string:mail>',methods=['GET'])
-def beer_by_user(mail) :
-    conn = connection.db_connection()
-    cursor= conn.cursor()
-    if request.method=='GET' :
-        cursor.execute("SELECT * FROM beer WHERE id in (SELECT beer_id from beerlist where user_id in (SELECT id FROM user WHERE mail =?))",(str(mail),))
-        beers = [
-            dict(id=row[0],name = row[1], percentageAlcohol=row[2], category=row[3])
-            for row in cursor.fetchall()
-        ]
-        cursor.close()
-        conn.close()
-        return jsonify(beers)
+@favorite_api.route('/favorite/<int:user_id>',methods=['GET'])
+def beer_by_user(user_id) :
+    token = request.headers.get('token')
+    if(verifyToken(token)) :
+        conn = connection.db_connection()
+        cursor= conn.cursor()
+        if request.method=='GET' :
+            cursor.execute("SELECT * FROM beer WHERE id in (SELECT beer_id from beerlist where user_id=?)",(str(user_id),))
+            beers = [
+                dict(id=row[0],name = row[1], percentageAlcohol=row[2], category=row[3])
+                for row in cursor.fetchall()
+            ]
+            cursor.close()
+            conn.close()
+            return jsonify(beers)
+    else :
+        return "Your token is expired"
 
-@favorite_api.route('/favorite',methods=['POST','DELETE'])
+@favorite_api.route('/favorite',methods=['POST'])
 def favorite():
-    conn = connection.db_connection()
-    cursor= conn.cursor()
-    if request.method=='POST' :
-        data = request.get_json()
-        beer_id=data['beer_id']
-        user_id = data['user_id']
-        sqllist = """INSERT INTO beerlist (user_id,beer_id) VALUES (?,?) """
-        cursor.execute(sqllist,(user_id,beer_id))
-        conn.commit()
-        cursor.close()
-        conn.close()
-        return f"Beer {beer_id} add to your favlist"
+    token = request.headers.get('token')
+    if(verifyToken(token)) :
+        conn = connection.db_connection()
+        cursor= conn.cursor()
+        if request.method=='POST' :
+            data = request.get_json()
+            beer_id=data['beer_id']
+            user_id = data['user_id']
+            sqllist = """INSERT INTO beerlist (user_id,beer_id) VALUES (?,?) """
+            cursor.execute(sqllist,(user_id,beer_id))
+            conn.commit()
+            cursor.close()
+            conn.close()
+            return f"Beer {beer_id} add to your favlist"
+    else :
+        return "Your token is expired"
 
 @favorite_api.route('/favorite/<int:beer_id>/<int:user_id>',methods=['DELETE'])
 def remove_from_favorite(beer_id,user_id):
-    conn = connection.db_connection()
-    cursor= conn.cursor()
-    if request.method=='DELETE' :
-        sqllist = """DELETE FROM beerlist where user_id = ? and beer_id=? """
-        cursor.execute(sqllist,(int(user_id),int(beer_id)))
-        conn.commit()
-        cursor.close()
-        conn.close()
-        return f"Beer {beer_id} remove from your favlist"
+    token = request.headers.get('token')
+    if(verifyToken(token)) :
+        conn = connection.db_connection()
+        cursor= conn.cursor()
+        if request.method=='DELETE' :
+            sqllist = """DELETE FROM beerlist where user_id = ? and beer_id=? """
+            cursor.execute(sqllist,(int(user_id),int(beer_id)))
+            conn.commit()
+            cursor.close()
+            conn.close()
+            return f"Beer {beer_id} remove from your favlist"
+    else :
+        return "Your token is expired"
--- Api/SearchApi.py
+++ Api/SearchApi.py
@@ -1,27 +1,33 @@
 from flask import Blueprint,request,jsonify
 import DatabaseFiles.connection as connection
+from VerifToken import verifyToken
+
 
 search_api = Blueprint('search_api',__name__)
 
 @search_api.route('/search/<string:name>',methods=['GET'])
 def search(name):
-    conn = connection.db_connection()
-    cursor= conn.cursor()
-    if request.method=='GET' :
-        cursor.execute("SELECT * FROM beer where name = ?",(str(name),))
-        rows = cursor.fetchall()
-        if(len(rows)>0) :
-            for r in rows :
-                beer = r
-            if beer is not None :
-                cursor.close()
-                conn.close()
-                return jsonify(beer),200
+    token = request.headers.get('token')
+    if(verifyToken(token)) :
+        conn = connection.db_connection()
+        cursor= conn.cursor()
+        if request.method=='GET' :
+            cursor.execute("SELECT * FROM beer where name = ?",(str(name),))
+            rows = cursor.fetchall()
+            if(len(rows)>0) :
+                for r in rows :
+                    beer = r
+                if beer is not None :
+                    cursor.close()
+                    conn.close()
+                    return jsonify(beer),200
+                else :
+                    cursor.close()
+                    conn.close()
+                    return "Something wrong",404
             else :
                 cursor.close()
                 conn.close()
                 return "Something wrong",404
-        else :
-            cursor.close()
-            conn.close()
-            return "Something wrong",404
+    else :
+        return "Your token is expired"
